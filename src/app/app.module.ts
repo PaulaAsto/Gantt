88c13104d7bba84f76a0a83b9f3e8d83677e8444
PaulaAsto <carolinaasto@gmail.com>
2017-12-26 18:48:35 -0500

    Actualizacion proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
 
+import { TimelineComponent }    from './timeline/timeline.component';
+import { ListActivityComponent }    from './timeline/listActivities/listActivities.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
+    ListActivityComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/timeline/listActivities/listActivities.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'list-activity',
+  templateUrl: './listActivities.component.html',
+  styleUrls: ['./listActivities.component.less']
+})
+export class ListActivityComponent {
+}
